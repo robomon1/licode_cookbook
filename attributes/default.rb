fe2839932a41e0277545f52de56b0355a0d1146c
robomon1 <robomon1@gmail.com>
2013-11-18 08:20:03 -0800

    Testing attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,3 +8,6 @@
 
 # Licode install (root) dir
 default[:licode_cookbook][:install_dir] = "/var/lib/licode"
+
+# Licode erizo controller defaults to first public ip
+default[:licode_cookbook][:erizo_controller] = node[:cloud][:public_ips][0] 
--- recipes/configLicode.rb
+++ recipes/configLicode.rb
@@ -18,7 +18,7 @@ end
 
 # Save the licode_config.js file
 template "#{node[:licode_cookbook][:install_dir]}/licode_config.js" do
-  source "licode_config_js"
+  source "licode_config_js.erb"
   cookbook "licode_cookbook"
   owner owner
   group group
